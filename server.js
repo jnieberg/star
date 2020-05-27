00f7da2c5d289bc6f0395d4eea1488b95dec6cbd
Jorg Nieberg <jorg.nieberg@kabisa.nl>
2020-05-27 13:55:57 +0200

    fix in server

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.get('/ping', (req, res) => {
 	return res.send('pong');
 });
 app.get('/*', (req, res) => {
-	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+	res.sendFile(path.join(__dirname, 'build', '/public/index.html'));
 });
 app.listen(port);
